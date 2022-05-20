da99a4565a88c5cf5a608160e8b67cb54215e680
Starsky <starsky.m@live.com>
2022-05-20 13:06:40 +0200

    Initial Commit

---
+++ ToDo_SwiftUI_CoreData/ToDo_SwiftUI_CoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_SwiftUI_CoreDataApp.swift
+//  ToDo_SwiftUI_CoreData
+//
+//  Created by star on 20.05.22.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_SwiftUI_CoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
