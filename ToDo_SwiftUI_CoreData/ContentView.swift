c5eece295405ea547fa845cd0413133682be9e7c
Starsky <starsky.m@live.com>
2022-05-23 14:12:07 +0200

    Preview and CoreData

--- ToDo_SwiftUI_CoreData/ContentView.swift
+++ ToDo_SwiftUI_CoreData/ContentView.swift
@@ -52,5 +52,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environment(\.managedObjectContext, CoreDataManager.preview.persistentContainer.viewContext)
     }
 }
--- ToDo_SwiftUI_CoreData/CoreDataManager.swift
+++ ToDo_SwiftUI_CoreData/CoreDataManager.swift
@@ -11,10 +11,32 @@ import CoreData
 class CoreDataManager {
     static let shared = CoreDataManager()
     
+    static var preview: CoreDataManager = {
+        let manager = CoreDataManager(inMemory: true)
+        let viewContext = manager.persistentContainer.viewContext
+        
+        for _ in 0..<2 {
+            let task = Task(context: viewContext)
+            task.id = UUID()
+            task.title = "Mustertitel \((0...50).randomElement()!)"
+            task.priority = (0...2).randomElement()!
+            task.timestamp = Date()
+        }
+        
+        
+        try? viewContext.save()
+        
+        return manager
+    }()
+    
     let persistentContainer: NSPersistentContainer
     
-    private init() {
+    private init(inMemory: Bool = false) {
         persistentContainer = NSPersistentContainer(name: "ToDo_SwiftUI_CoreData")
+        
+        if inMemory {
+            persistentContainer.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+        }
         
         persistentContainer.loadPersistentStores { (_, error) in
             if let error = error {
